d36defc3da4bc1379dab6933112a16ae2470f12a
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-21 22:51:44 -0400

    added some model files

---
+++ Capstone/Models/Application.swift
@@ -0,0 +1,9 @@
+//
+//  Application.swift
+//  Capstone
+//
+//  Created by Amy Alsaydi on 5/21/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
---
+++ Capstone/Models/Job.swift
@@ -0,0 +1,9 @@
+//
+//  Job.swift
+//  Capstone
+//
+//  Created by Amy Alsaydi on 5/21/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
