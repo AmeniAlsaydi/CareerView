f99cea1cbcb20d94c1d464872f38be11d54779a6
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-26 09:14:59 -0400

    tested firebase

---
+++ CapstoneTests/CapstoneTests.swift
@@ -0,0 +1,31 @@
+//
+//  CapstoneTests.swift
+//  CapstoneTests
+//
+//  Created by Amy Alsaydi on 5/26/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import XCTest
+
+@testable import Capstone
+
+class CapstoneTests: XCTestCase {
+    
+    func testFirebase() {
+        let expected = "hello"
+        let exp = XCTestExpectation(description: "test found")
+        
+        DatabaseService.shared.testFirebase { (results) in
+            switch results {
+                case(.failure(let error)):
+                    print("error in test: \(error.localizedDescription)")
+                case(.success(let tests)):
+                    XCTAssertEqual(expected, tests.first?.test)
+                    exp.fulfill()
+            }
+        }
+        wait(for:[exp], timeout: 9.0)
+    }
+    
+}
