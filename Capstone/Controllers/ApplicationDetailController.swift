1b294f0ac8830096e16ad85856fc7dda98cadcfd
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-26 14:47:46 -0400

    added view controller files

---
+++ Capstone/Controllers/ApplicationDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  ApplicationDetailController.swift
+//  Capstone
+//
+//  Created by Amy Alsaydi on 5/26/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
