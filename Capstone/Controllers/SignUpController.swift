a3b0a353289d4dd556571394c4dbeb6925ad9921
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-27 09:45:04 -0400

    added log in VCs

---
+++ Capstone/Controllers/SignUpController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpController.swift
+//  Capstone
+//
+//  Created by Amy Alsaydi on 5/27/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
