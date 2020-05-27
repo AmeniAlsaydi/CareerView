c3b3d600f6507f2a08d45d36e0d81e1fe6de2d66
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-27 09:29:13 -0400

    Set up tab bar VC classes and storyboard IDs.

--- Capstone/Controllers/SettingsController.swift
+++ Capstone/Controllers/SettingsController.swift
@@ -1,18 +1,18 @@
 //
-//  InterviewQuestionsMainController.swift
+//  SettingsController.swift
 //  Capstone
 //
-//  Created by Amy Alsaydi on 5/26/20.
+//  Created by Amy Alsaydi on 5/27/20.
 //  Copyright © 2020 Amy Alsaydi. All rights reserved.
 //
 
 import UIKit
 
-class InterviewQuestionsMainController: UIViewController {
+class SettingsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+   
 }
