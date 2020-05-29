6de5a0eb9d510247c2b711d0500bf4a726f44cd5
Casandra <casandragrullon@pursuit.org>
2020-05-29 17:24:42 -0400

    added table view and basic cell for job history main vc

---
+++ Capstone/Views/JobHistoryBasicCell.swift
@@ -0,0 +1,17 @@
+//
+//  JobHistoryBasicCell.swift
+//  Capstone
+//
+//  Created by casandra grullon on 5/29/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class JobHistoryBasicCell: UITableViewCell {
+    @IBOutlet weak var jobTitleLabel: UILabel!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var jobDescriptionLabel: UILabel!
+    @IBOutlet weak var datesLabel: UILabel!
+    
+}
