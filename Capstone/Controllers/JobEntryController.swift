7a80c1e993b0aec2853df9465231bbff0dfe8f9f
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-05-28 23:15:38 -0400

    Added viewController to xib and started troubleshooting build

--- Capstone/Controllers/JobEntryController.swift
+++ Capstone/Controllers/JobEntryController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class JobEntryController: UIViewController {
 
+    @IBOutlet var jobTitleCell: UITableViewCell!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-   
+ 
 
+}
+extension JobEntryController: UITableViewDataSource {
+     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+         switch section {
+         case 0:
+             return "Job Title"
+         default:
+             return "What?"
+         }
+     }
+      func numberOfSections(in tableView: UITableView) -> Int {
+         return 1
+     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 1
+     }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         if indexPath.section == 0 {
+             if indexPath.row == 0 {
+                 let cell = tableView.dequeueReusableCell(withIdentifier: "jobTitleCell")!
+                 return cell
+             }
+         }
+         return jobTitleCell
+     }
+}
+extension JobEntryController: UITableViewDelegate {
+    
 }
