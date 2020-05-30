9a683799f441dfa5d86d6edf5c693a0c41b95e68
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-05-30 13:59:27 -0400

    Added some keyboard handling

--- Capstone/Controllers/JobEntryController.swift
+++ Capstone/Controllers/JobEntryController.swift
@@ -25,25 +25,35 @@ class JobEntryController: UIViewController {
     @IBOutlet var responsiblity3Cell: UITableViewCell!
     @IBOutlet var addStarSituationCell: UITableViewCell!
     @IBOutlet var addContactCell: UITableViewCell!
-    
-    @IBOutlet weak var jobTitleTextField: UITextField!
-    @IBOutlet weak var companyNameTextField: UITextField!
-    
+    //MARK:- Buttons
     @IBOutlet weak var currentlyEmployedButton: UIButton!
-    
-    @IBOutlet weak var beginDateMonthTextField: UITextField!
-    @IBOutlet weak var beginDateYearTextField: UITextField!
-    @IBOutlet weak var endDateMonthTextField: UITextField!
-    @IBOutlet weak var endDateYearTextField: UITextField!
     @IBOutlet weak var addResponsibilityButton: UIButton!
     @IBOutlet weak var addStarSituationButton: UIButton!
     @IBOutlet weak var addContactButton: UIButton!
+    //MARK:- TextFields
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var companyNameTextField: UITextField!
+    @IBOutlet weak var beginDateMonthTextField: UITextField!
+    @IBOutlet weak var beginDateYearTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var endDateMonthTextField: UITextField!
+    @IBOutlet weak var endDateYearTextField: UITextField!
+    @IBOutlet weak var responsibility1TextField: UITextField!
+    @IBOutlet weak var responsibilty2TextField: UITextField!
+    @IBOutlet weak var responsibility3TextField: UITextField!
+    
     
     //MARK:- Variables
     public var userJob: UserJob?
     private var currentlyEmployed: Bool = false {
         didSet {
             configureCurrentlyEmployedButton(currentlyEmployed)
+        }
+    }
+    private var numberOfRows = 12 {
+        didSet {
+            
         }
     }
     
@@ -53,6 +63,7 @@ class JobEntryController: UIViewController {
         configureView()
         setupTextFields()
         setupNavigationBar()
+        listenForKeyboardEvents()
     }
     private func configureView() {
         view.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
@@ -66,20 +77,6 @@ class JobEntryController: UIViewController {
         navigationItem.rightBarButtonItem = rightBarButton
     }
     private func setupTextFields() {
-        jobTitleTextField.setPadding()
-        jobTitleTextField.setBottomBorder()
-        companyNameTextField.setPadding()
-        companyNameTextField.setBottomBorder()
-        
-        beginDateMonthTextField.setPadding()
-        beginDateMonthTextField.setBottomBorder()
-        beginDateYearTextField.setPadding()
-        beginDateYearTextField.setBottomBorder()
-        endDateMonthTextField.setPadding()
-        endDateMonthTextField.setBottomBorder()
-        endDateYearTextField.setPadding()
-        endDateYearTextField.setBottomBorder()
-        
         let toolbar = UIToolbar(frame: CGRect(origin: .zero,
                                               size: .init(width: view.frame.size.width, height: 30)))
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
@@ -90,13 +87,26 @@ class JobEntryController: UIViewController {
                                          action: #selector(doneButtonAction))
         toolbar.setItems([flexSpace, doneButton], animated: false)
         toolbar.sizeToFit()
-        jobTitleTextField.inputAccessoryView = toolbar
-        companyNameTextField.inputAccessoryView = toolbar
-        beginDateMonthTextField.inputAccessoryView = toolbar
-        beginDateYearTextField.inputAccessoryView = toolbar
-        endDateMonthTextField.inputAccessoryView = toolbar
-        endDateYearTextField.inputAccessoryView = toolbar
+        
+        let textFields = [jobTitleTextField, companyNameTextField, beginDateYearTextField, beginDateMonthTextField, endDateYearTextField, endDateMonthTextField, locationTextField, descriptionTextField, responsibility1TextField, responsibilty2TextField, responsibility3TextField]
+        for field in textFields {
+            field?.delegate = self
+            field?.inputAccessoryView = toolbar
+            field?.setPadding()
+            field?.setBottomBorder()
+        }
     }
+    private func listenForKeyboardEvents() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
     @objc func doneButtonAction() {
         self.view.endEditing(true)
     }
@@ -116,7 +126,21 @@ class JobEntryController: UIViewController {
     @IBAction func currentlyEmployedButtonPressed(_ sender: UIButton) {
         currentlyEmployed.toggle()
     }
-    
+    @objc func keyboardWillChange(notification: Notification) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+        view.frame.origin.y = -keyboardRect.height
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
+    private func scrollToRow(row: Int) {
+        let indexPath = IndexPath(row: row, section: 0)
+        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
 }
 //MARK:- TableViewController Datasource/Delegate
 extension JobEntryController: UITableViewDataSource {
@@ -132,7 +156,7 @@ extension JobEntryController: UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return numberOfRows
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
@@ -147,6 +171,20 @@ extension JobEntryController: UITableViewDataSource {
             return beginEmploymentDateCell
         case 4:
             return endEmploymentCell
+        case 5:
+            return locationCell
+        case 6:
+            return descriptionCell
+        case 7:
+            return mainResponsiblityCell
+        case 8:
+            return responsiblity2Cell
+        case 9:
+            return responsiblity3Cell
+        case 10:
+            return addStarSituationCell
+        case 11:
+            return addContactCell
         default:
             return jobTitleCell
         }
@@ -157,15 +195,20 @@ extension JobEntryController: UITableViewDataSource {
 extension JobEntryController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 2 {
-            currentlyEmployed.toggle()        }
+            currentlyEmployed.toggle()
+            
+        }
     }
 }
 
 //MARK:- UITextField Delegate
 extension JobEntryController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let row = textField.tag
+        scrollToRow(row: row)
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-//        self.view.endEditing(true)
         return true
     }
 }
@@ -173,14 +216,16 @@ extension JobEntryController: UITextFieldDelegate {
 //MARK:- UITextField Extension
 extension UITextField {
     func setPadding() {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = .always
+        self.layer.backgroundColor = UIColor.white.cgColor
     }
     func setBottomBorder() {
         self.layer.shadowColor = UIColor.systemGray.cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
         self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0.0
+        self.layer.backgroundColor = UIColor.white.cgColor
     }
 }
