47369c75648dcd0f0f916fcb68a1e2afabb81816
Casandra <casandragrullon@pursuit.org>
2020-06-01 10:10:44 -0400

    added labels and buttons to basic job history cell

--- Capstone/Views/JobHistoryBasicCell.swift
+++ Capstone/Views/JobHistoryBasicCell.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
+class ContactCell: UICollectionViewCell {
+    @IBOutlet weak var contactNameButton: UIButton!
+    
+    @IBAction func contactButtonPressed(_ sender: UIButton) {
+        
+    }
+}
+
 class JobHistoryBasicCell: UITableViewCell {
     @IBOutlet weak var jobTitleLabel: UILabel!
     @IBOutlet weak var companyNameLabel: UILabel!
     @IBOutlet weak var jobDescriptionLabel: UILabel!
     @IBOutlet weak var datesLabel: UILabel!
- 
-    //add responsibilities labels 3 - 5 
-    //place to segue to situations
-    //place to segue to contacts
-    //add edit button
-    //resizing cell
+    @IBOutlet weak var responsibilitiesLabel1: UILabel!
+    @IBOutlet weak var responsibilitiesLabel2: UILabel!
+    @IBOutlet weak var responsibilitiesLabel3: UILabel!
+    @IBOutlet weak var responsibilitiesLabel4: UILabel!
+    @IBOutlet weak var responsibilitiesLabel5: UILabel!
+    @IBOutlet weak var starSituationsButton: UIButton!
+    @IBOutlet weak var contactsCollectionView: UICollectionView!
     
+    @IBAction func starSituationButtonPressed(_ sender: UIButton) {
+        //Segue to STAR Story VC
+    }
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        //Segue to JobEntry VC editing mode
+    }
 }
+//TODO: make this cell resizing
+
