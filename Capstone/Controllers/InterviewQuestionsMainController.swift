9fcdd371ba6619515ba354f4ed7be645c4df764d
Casandra <casandragrullon@pursuit.org>
2020-06-01 16:45:22 -0400

    added interview cell to proj file, grabbed common interview question from firebase

--- Capstone/Controllers/InterviewQuestionsMainController.swift
+++ Capstone/Controllers/InterviewQuestionsMainController.swift
@@ -12,9 +12,64 @@ class InterviewQuestionsMainController: UIViewController {
 
     @IBOutlet weak var questionsCollectionView: UICollectionView!
     
+    private var interviewQuestions = [InterviewQuestion]() {
+        didSet {
+            self.questionsCollectionView.reloadData()
+        }
+    }
+    //public var userAnswers = [InterviewAnswer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureCollectionView()
+        getInterviewQuestions()
+        getUserAnswers()
     }
+    private func configureCollectionView() {
+        questionsCollectionView.delegate = self
+        questionsCollectionView.dataSource = self
+        questionsCollectionView.register(UINib(nibName: "InterviewQuestionCellXib", bundle: nil), forCellWithReuseIdentifier: "interviewQuestionCell")
+    }
+    private func getInterviewQuestions() {
+        DatabaseService.shared.fetchCommonInterviewQuestions { [] (result) in
+            switch result {
+            case .failure(let error) :
+                print("could not fetch common interview questions from firebase error: \(error.localizedDescription)")
+            case .success(let questions):
+                DispatchQueue.main.async {
+                    self.interviewQuestions = questions
+                }
+            }
+        }
+    }
+    private func getUserAnswers() {
+        //TODO: need user answers to populate the interview cell with number of star stories and answers
+    }
+    
+    
+}
+extension InterviewQuestionsMainController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxsize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxsize.width * 0.9
+        let itemHeight: CGFloat = maxsize.height * 0.15
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+}
+extension InterviewQuestionsMainController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return interviewQuestions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = questionsCollectionView.dequeueReusableCell(withReuseIdentifier: "interviewQuestionCell", for: indexPath) as? InterviewQuestionCell else {
+            fatalError("could not cast to interviewquestioncell")
+        }
+        let question = interviewQuestions[indexPath.row]
+        //let answer = userAnswers[indexPath.row]
+        cell.configureCell(interviewQ: question)
+        return cell
+    }
+    
     
 }
