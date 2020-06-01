ee89674ab39ab2a52b0797fc48fbf58e7badb129
Casandra <casandragrullon@pursuit.org>
2020-06-01 17:18:22 -0400

    added nav button to create a new interview question

--- Capstone/Views/InterviewQuestionCell.swift
+++ Capstone/Views/InterviewQuestionCell.swift
@@ -13,6 +13,12 @@ class InterviewQuestionCell: UICollectionViewCell {
     @IBOutlet weak var answerCheckBox: UIImageView!
     @IBOutlet weak var interviewQuestionLabel: UILabel!
     
+    override func layoutSubviews() {
+        self.layer.borderWidth = 2
+        //let purple = #colorLiteral(red: 0.305962503, green: 0.1264642179, blue: 0.6983457208, alpha: 1)
+        //self.layer.borderColor = purple as! CGColor
+        self.layer.cornerRadius = 13
+    }
     public func configureCell(interviewQ: InterviewQuestion, answer: InterviewAnswer? = nil) {
         interviewQuestionLabel.text = interviewQ.question
         numberOfStarsLabel.text = answer?.starSituationIDs.count.description ?? "0"
