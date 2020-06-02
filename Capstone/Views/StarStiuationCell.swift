12654cf3751fa62c06fc5c69123bc3ad7e966e70
Casandra <casandragrullon@pursuit.org>
2020-06-01 22:55:58 -0400

    working on answer question vc ui

---
+++ Capstone/Views/StarStiuationCell.swift
@@ -0,0 +1,27 @@
+//
+//  StarStiuationCell.swift
+//  Capstone
+//
+//  Created by casandra grullon on 6/1/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class StarStiuationCell: UICollectionViewCell {
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var jobTitleLabel: UILabel!
+    @IBOutlet weak var numberOfQuestions: UILabel!
+    @IBOutlet weak var situationLabel: UILabel!
+    
+    public func configureCell(starSituation: StarSituation) {
+        jobTitleLabel.text = starSituation.userJob.title
+        numberOfQuestions.text = "Answers \(starSituation.interviewQuestions.count) questions"
+        situationLabel.text = starSituation.situation
+    }
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        //TODO: segues to StarStoryEntryVC
+        //TODO: create delegate
+    }
+}
