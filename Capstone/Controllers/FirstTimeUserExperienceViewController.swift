2adc636991d95f54667780738d1dbb2b2f587a8c
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-03 12:25:43 -0400

    Added xib for and view controller file for FirsTimeUserExperience, formatted check for first time log in better

---
+++ Capstone/Controllers/FirstTimeUserExperienceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTimeUserExperienceViewController.swift
+//  Capstone
+//
+//  Created by Gregory Keeley on 6/3/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class FirstTimeUserExperienceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
