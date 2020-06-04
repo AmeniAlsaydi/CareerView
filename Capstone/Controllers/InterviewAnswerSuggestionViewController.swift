bc60d8c65be344372a97109eadadda7f13382a9d
Casandra <casandragrullon@pursuit.org>
2020-06-04 11:28:32 -0400

    suggestion presents modally

--- Capstone/Controllers/InterviewAnswerSuggestionViewController.swift
+++ Capstone/Controllers/InterviewAnswerSuggestionViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class InterviewAnswerSuggestionViewController: UIViewController {
-
+    
     @IBOutlet weak var suggestionLabel: UILabel!
     
     var interviewQuestion: InterviewQuestion?
     
     override func viewDidLoad() {
+        //halfFrame()
         configureLabel(for: interviewQuestion)
     }
+    
     public func configureLabel(for question: InterviewQuestion?) {
         suggestionLabel.text = question?.suggestion
     }
     
+//    public func halfFrame() {
+//        let frame = UIScreen.main.bounds
+//        let height: CGFloat = frame.height * 0.5
+//        let width: CGFloat = frame.width
+//        let x: CGFloat = frame.minX
+//        let y: CGFloat = frame.midY
+//        view.frame = CGRect(x: x, y: y, width: width, height: height)
+//    }
 }
