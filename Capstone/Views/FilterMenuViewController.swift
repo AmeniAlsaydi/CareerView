5f225b713406a73eab53cb72da4b56f40152a59a
Casandra <casandragrullon@pursuit.org>
2020-06-06 19:59:57 -0400

    started filter menu functions

--- Capstone/Views/FilterMenuViewController.swift
+++ Capstone/Views/FilterMenuViewController.swift
@@ -15,26 +15,58 @@ class FilterMenuViewController: UIViewController {
     @IBOutlet weak var commonButton: UIButton!
     @IBOutlet weak var customButton: UIButton!
     
+    public var filterState: FilterState = .all
+    
     override func viewDidLoad() {
-
+        updateUI()
+        halfFrame()
+    }
+    private func halfFrame() {
+        let width = view.frame.width / 2
+        view.frame = CGRect(x: view.frame.minX, y: view.frame.minY, width: width, height: view.frame.height)
+    }
+    private func updateUI() {
+        if filterState == .all {
+            allButton.setImage(UIImage(systemName: "checkmark.rectangle.fill"), for: .normal)
+            savedButton.setImage(UIImage(systemName: "square"), for: .normal)
+            commonButton.setImage(UIImage(systemName: "square"), for: .normal)
+            customButton.setImage(UIImage(systemName: "square"), for: .normal)
+        } else if filterState == .saved {
+            savedButton.setImage(UIImage(systemName: "checkmark.rectangle.fill"), for: .normal)
+            allButton.setImage(UIImage(systemName: "square"), for: .normal)
+            commonButton.setImage(UIImage(systemName: "square"), for: .normal)
+            customButton.setImage(UIImage(systemName: "square"), for: .normal)
+        } else if filterState == .common {
+            commonButton.setImage(UIImage(systemName: "checkmark.rectangle.fill"), for: .normal)
+            allButton.setImage(UIImage(systemName: "square"), for: .normal)
+            savedButton.setImage(UIImage(systemName: "square"), for: .normal)
+            customButton.setImage(UIImage(systemName: "square"), for: .normal)
+        } else if filterState == .custom {
+            customButton.setImage(UIImage(systemName: "checkmark.rectangle.fill"), for: .normal)
+            allButton.setImage(UIImage(systemName: "square"), for: .normal)
+            savedButton.setImage(UIImage(systemName: "square"), for: .normal)
+            commonButton.setImage(UIImage(systemName: "square"), for: .normal)
+        }
     }
     
     @IBAction func allButtonPressed(_ sender: UIButton) {
-        
+        filterState = .all
     }
     @IBAction func savedButtonPressed(_ sender: UIButton){
-        
+        filterState = .saved
     }
     @IBAction func commonButtonPressed(_ sender: UIButton) {
-        
+        filterState = .common
     }
     @IBAction func customButtonPressed(_ sender: UIButton) {
-        
+        filterState = .custom
     }
     @IBAction func setFilterButtonPressed(_ sender: UIButton) {
-        
+        let interviewQuestionsVC = InterviewQuestionsMainController()
+        interviewQuestionsVC.filterState = filterState
+        dismiss(animated: true)
     }
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
-        
+        dismiss(animated: true)
     }
 }
