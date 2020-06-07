c954f78487877cddc8a51b275ff5565165ace464
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-06-07 19:18:03 -0400

    began working on application collection view

---
+++ Capstone/Views/JobApplicationCell.swift
@@ -0,0 +1,13 @@
+//
+//  JobApplicationCell.swift
+//  Capstone
+//
+//  Created by Amy Alsaydi on 6/7/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class JobApplicationCell: UICollectionViewCell {
+    
+}
