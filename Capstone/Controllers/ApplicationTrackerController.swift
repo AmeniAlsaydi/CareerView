31de68f421d342f6ac61c6f4c2f0b3dafd476b44
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-06-07 19:18:03 -0400

    began working on application collection view

--- Capstone/Controllers/ApplicationTrackerController.swift
+++ Capstone/Controllers/ApplicationTrackerController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ApplicationTrackerController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        
+    }
+    
+    private func configureCollectionView() {
+        collectionView.backgroundView = EmptyView(title: "hello", message: "test", imageName: "mic")
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
     }
     
 
 
+}
+
+extension ApplicationTrackerController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
+
+extension ApplicationTrackerController: UICollectionViewDelegateFlowLayout {
+    
+    // height and width
+    
 }
