9130d18509678cd1708ba93a7dfea264603b3770
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-07 19:28:40 -0400

    Views are now purple

--- Capstone/Controllers/StarStoryEntryController.swift
+++ Capstone/Controllers/StarStoryEntryController.swift
@@ -27,16 +27,22 @@ class StarStoryEntryController: UIViewController {
     @IBOutlet weak var actionTextViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var resultTextViewHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var situationBkgdView: UIView!
+    @IBOutlet weak var taskBkgdView: UIView!
+    @IBOutlet weak var actionBkgdView: UIView!
+    @IBOutlet weak var resultBkgdView: UIView!
+    
     private var saveChoiceAsDefault = true
+    
+    private var starSituation: StarSituation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
-        setTextViewHeights()
     }
     private func configureView() {
         navigationController?.navigationBar.topItem?.title = "Back"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.down.fill"), style: .plain, target: self, action: #selector(saveButtonPressed(_:)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveButtonPressed(_:)))
         
         starStoryButton.layer.borderWidth = CGFloat(1.0)
         freeFormButton.layer.borderWidth = CGFloat(1.0)
@@ -56,23 +62,31 @@ class StarStoryEntryController: UIViewController {
         taskTextView.layer.cornerRadius = 4
         actionTextView.layer.cornerRadius = 4
         resultTextView.layer.cornerRadius = 4
-        situationTextView.layer.masksToBounds = true
-        taskTextView.layer.masksToBounds = true
-        resultTextView.layer.masksToBounds = true
-        resultTextView.layer.masksToBounds = true
-
-
+        setTextViewHeights()
+        
+        configureTextViews(view: situationBkgdView)
+        configureTextViews(view: taskBkgdView)
+        configureTextViews(view: actionBkgdView)
+        configureTextViews(view: resultBkgdView)
+        
+    }
+    private func configureTextViews(view: UIView) {
+//        view.layer.shadowColor = UIColor.black.cgColor
+//        view.layer.shadowOpacity = 1
+//        view.layer.shadowOffset = .zero
+//        view.layer.shadowRadius = 0.25
+        view.layer.cornerRadius = 4
     }
     private func setTextViewHeights() {
         situationTextView.sizeToFit()
         situationTextViewHeightConstraint.constant = situationTextView.contentSize.height
-        
+
         taskTextView.sizeToFit()
         taskTextViewHeightContraint.constant = taskTextView.contentSize.height
-        
+
         actionTextView.sizeToFit()
         actionTextViewHeightConstraint.constant = actionTextView.contentSize.height
-        
+
         resultTextView.sizeToFit()
         resultTextViewHeightConstraint.constant = resultTextView.contentSize.height
     }
@@ -102,6 +116,22 @@ class StarStoryEntryController: UIViewController {
         } else {
             saveAsDefaultButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
         }
+    }
+    @IBAction func clearSituationButtonPressed(_ sender: UIButton) {
+        situationTextView.text = ""
+        setTextViewHeights()
+    }
+    @IBAction func clearTaskButtonPressed(_ sender: UIButton) {
+        taskTextView.text = ""
+        setTextViewHeights()
+    }
+    @IBAction func clearActionButtonPressed(_ sender: UIButton) {
+        actionTextView.text = ""
+        setTextViewHeights()
+    }
+    @IBAction func clearResultButtonPressed(_ sender: UIButton) {
+        resultTextView.text = ""
+        setTextViewHeights()
     }
     
 }
