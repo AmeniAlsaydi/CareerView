fb48d1ebba74869b9d431a5b7c325f7a92d4f4fb
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-07 10:04:31 -0400

    Added save choice as default button, and save button

--- Capstone/Controllers/StarStoryEntryController.swift
+++ Capstone/Controllers/StarStoryEntryController.swift
@@ -12,8 +12,11 @@ class StarStoryEntryController: UIViewController {
 
     @IBOutlet weak var starStoryButton: UIButton!
     @IBOutlet weak var freeFormButton: UIButton!
+    @IBOutlet weak var saveAsDefaultButton: UIButton!
     
     @IBOutlet weak var inputOptionView: UIView!
+    
+    private var saveChoiceAsDefault = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,7 @@ class StarStoryEntryController: UIViewController {
     }
     private func configureView() {
         navigationController?.navigationBar.topItem?.title = "Back"
-        navigationItem.rightBarButtonItem?.setBackgroundImage(UIImage(systemName: "lightbulb.fill"), for: .normal, style: .plain, barMetrics: .default)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.down.fill"), style: .plain, target: self, action: #selector(saveButtonPressed(_:)))
         
         starStoryButton.layer.borderWidth = CGFloat(1.0)
         freeFormButton.layer.borderWidth = CGFloat(1.0)
@@ -32,4 +35,24 @@ class StarStoryEntryController: UIViewController {
         inputOptionView.layer.cornerRadius = 4
         inputOptionView.layer.masksToBounds = true
     }
+    @objc private func saveButtonPressed(_ sender: UIBarButtonItem) {
+        print("Save pressed")
+    }
+    @IBAction func freeFormButtonPressed(_ sender: UIButton) {
+        print("Free form button pressed")
+        inputOptionView.isHidden = true
+    }
+    @IBAction func starStoryButtonPressed(_ sender: UIButton) {
+        print("Star story button pressed")
+        inputOptionView.isHidden = true
+    }
+    @IBAction func saveAsDefaultButtonPressed(_ sender: UIButton) {
+        saveChoiceAsDefault.toggle()
+        if saveChoiceAsDefault {
+            saveAsDefaultButton.setImage(UIImage(systemName: "square"), for: .normal)
+        } else {
+            saveAsDefaultButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+        }
+    }
+    
 }
