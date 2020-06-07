fcc82fc23c88a93a6838047a9490969c63cabe25
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-07 09:42:12 -0400

    Added a view with option for STAR story input, or Free Form

--- Capstone/Controllers/StarStoryEntryController.swift
+++ Capstone/Controllers/StarStoryEntryController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class StarStoryEntryController: UIViewController {
 
+    @IBOutlet weak var starStoryButton: UIButton!
+    @IBOutlet weak var freeFormButton: UIButton!
+    
+    @IBOutlet weak var inputOptionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
     }
     private func configureView() {
-        navigationItem.backBarButtonItem?.title = "Back"
+        navigationController?.navigationBar.topItem?.title = "Back"
+        navigationItem.rightBarButtonItem?.setBackgroundImage(UIImage(systemName: "lightbulb.fill"), for: .normal, style: .plain, barMetrics: .default)
+        
+        starStoryButton.layer.borderWidth = CGFloat(1.0)
+        freeFormButton.layer.borderWidth = CGFloat(1.0)
+        starStoryButton.layer.cornerRadius = 4
+        starStoryButton.layer.masksToBounds = true
+        freeFormButton.layer.cornerRadius = 4
+        freeFormButton.layer.masksToBounds = true
+        inputOptionView.layer.cornerRadius = 4
+        inputOptionView.layer.masksToBounds = true
     }
 }
