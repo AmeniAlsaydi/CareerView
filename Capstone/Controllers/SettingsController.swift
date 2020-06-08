3fb9a49801d8a5ee2e3e3addc1e1cec24ec2354f
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-08 09:41:14 -0400

    Added dependancy for DataPersistence, added UserDefaults, added option to show option for Star Story Input method, added option in settings to toggle Star situation Input method

--- Capstone/Controllers/SettingsController.swift
+++ Capstone/Controllers/SettingsController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class SettingsController: UIViewController {
-
+    
+    @IBOutlet weak var starSituationInputToggle: UISwitch!
+    
+    private var showUserStarSituationInputOption = ShowUserStarInputOption.on {
+        didSet {
+            if showUserStarSituationInputOption.rawValue == ShowUserStarInputOption.off.rawValue {
+                starSituationInputToggle.isOn = false
+            } else {
+                starSituationInputToggle.isOn = true
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
-   
+    private func loadSettings() {
+        if let showStarSituationInputOption = UserPreference.shared.getPreferenceShowInputOption() {
+            showUserStarSituationInputOption = showStarSituationInputOption
+        }
+        
+    }
+    @IBAction func starSituationInputOptionToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            showUserStarSituationInputOption = ShowUserStarInputOption.on
+            UserPreference.shared.updatePreferenceShowUserInputOption(with: showUserStarSituationInputOption)
+        } else {
+            
+            showUserStarSituationInputOption = ShowUserStarInputOption.off
+            UserPreference.shared.updatePreferenceShowUserInputOption(with: showUserStarSituationInputOption)
+        }
+        print("Star Situation Input option: \(showUserStarSituationInputOption.rawValue)")
+    }
+    
 }
