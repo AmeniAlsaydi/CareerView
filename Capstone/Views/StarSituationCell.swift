83c6a496e701fad0c1a36b836572f8d14ed987e0
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-06-08 17:31:09 -0400

    Fixed typo in StarSituationCell name, added long press gesture to star situation cell and delete function

--- Capstone/Views/StarSituationCell.swift
+++ Capstone/Views/StarSituationCell.swift
@@ -8,22 +8,45 @@
 
 import UIKit
 
-class StarStiuationCell: UICollectionViewCell {
+protocol StarSituationCellDelegate: AnyObject {
+    func longPressOnStarSituation(starSituation: StarSituation, starSituationCell: StarSituationCell)
+}
+
+class StarSituationCell: UICollectionViewCell {
+    
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var jobTitleLabel: UILabel!
     @IBOutlet weak var numberOfQuestions: UILabel!
     @IBOutlet weak var situationLabel: UILabel!
+    
+    weak var delegate: StarSituationCellDelegate?
+    private var starSituationForDelegate: StarSituation?
+    
+    private lazy var longPressGesture: UILongPressGestureRecognizer = {
+        let gesture = UILongPressGestureRecognizer()
+        gesture.addTarget(self, action: #selector(longPressAction(gesture:)))
+        return gesture
+    }()
+    @objc private func longPressAction(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            gesture.state = .cancelled
+            return
+        }
+        delegate?.longPressOnStarSituation(starSituation: starSituationForDelegate!, starSituationCell: self)
+    }
     
     override func layoutSubviews() {
         self.layer.borderWidth = 2
         //let purple = #colorLiteral(red: 0.305962503, green: 0.1264642179, blue: 0.6983457208, alpha: 1)
         //self.layer.borderColor = purple as! CGColor
         self.layer.cornerRadius = 13
+        addGestureRecognizer(longPressGesture)
     }
     public func configureCell(starSituation: StarSituation) {
         jobTitleLabel.text = starSituation.userJobID
         numberOfQuestions.text = "\(starSituation.interviewQuestionsIDs.count) questions"
         situationLabel.text = starSituation.situation
+        starSituationForDelegate = starSituation
     }
     
     @IBAction func editButtonPressed(_ sender: UIButton) {
