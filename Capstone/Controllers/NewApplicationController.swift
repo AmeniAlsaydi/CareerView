ae6df84d9c4de55ce4fb02a86e6753b9f6a3b9a9
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-06-09 20:21:01 -0400

    began work on basic UI for application entry VC

--- Capstone/Controllers/NewApplicationController.swift
+++ Capstone/Controllers/NewApplicationController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class NewApplicationController: UIViewController {
 
+    @IBOutlet weak var companyNameTextField: UITextField!
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var notesLabel: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        styleAllTextFields()
+        
 
-        // Do any additional setup after loading the view.
     }
+    
+    
+    private func styleAllTextFields() {
+        styleTextField(companyNameTextField)
+        styleTextField(jobTitleTextField)
+        styleTextField(linkTextField)
+        styleTextField(locationTextField)
+        styleTextField(notesLabel)
+    }
+    
+    
+    private func styleTextField(_ textfield: UITextField) {
+        
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 1)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        textfield.borderStyle = .none
+        textfield.layer.addSublayer(bottomLine)
+    }
+    
+    @IBAction func hasAppliedButtonChecked(_ sender: UIButton) {
+        
+        // animate and display the date applied stack
+        
+        
+        
+    }
+    
     
 
 }
