f7e4b0121227edb1f54d4d5608aed231eef5c652
GregKeeley <GregoryKeeley@pursuit.org>
2020-06-23 16:24:07 -0400

    Added welcomeScreen xib and view controller, with some basic info

--- Capstone/Controllers/JobHistoryController.swift
+++ Capstone/Controllers/JobHistoryController.swift
@@ -12,8 +12,6 @@ class JobHistoryController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    private var userData: User?
-    
     private var displayContactCollectionView = false
     
     var userJobHistory = [UserJob]() {
@@ -34,10 +32,6 @@ class JobHistoryController: UIViewController {
         super.viewDidLoad()
         configureTableView()
         configureNavBar()
-        
-        getUserData()
-        checkFirstTimeLogin()
-        
         loadUserJobs()
         setup()
     }
@@ -62,32 +56,6 @@ class JobHistoryController: UIViewController {
         let jobEntryController = NewJobEntryController(nibName: "NewJobEntryXib", bundle: nil)
         jobEntryController.editingJob = false
         show(jobEntryController, sender: nil)
-    }
-    private func getUserData() {
-        DatabaseService.shared.fetchUserData { [weak self] (result) in
-            switch result {
-            case .failure(let error):
-                DispatchQueue.main.async {
-                    print("Error fetching user Data: \(error.localizedDescription)")
-                }
-            case .success(let userData):
-                DispatchQueue.main.async {
-                    self?.userData = userData
-                    self?.checkFirstTimeLogin()
-                }
-            }
-        }
-    }
-    private func checkFirstTimeLogin() {
-        guard let user = userData else { return }
-        if user.firstTimeLogin {
-            print("First time logging in")
-            //Eventually move this to the viewcontroller file once the user has completed the on boarding experience
-            let firstTimeUserExperienceViewController = FirstTimeUserExperienceViewController(nibName: "FirstTimeUserExperienceViewControllerXib", bundle: nil)
-            show(firstTimeUserExperienceViewController, sender: nil)
-        } else {
-            print("User has logged in before")
-        }
     }
     private func loadUserJobs() {
         DatabaseService.shared.fetchUserJobs { (result) in
