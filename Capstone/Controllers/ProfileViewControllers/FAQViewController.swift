9ae9a79c22c0a142c1c20f9e8dee16535db7e40b
Casandra <casandragrullon@gmail.com>
2020-06-27 11:08:27 -0400

    edited FAQinfo grammar and spelling errors

--- Capstone/Controllers/ProfileViewControllers/FAQViewController.swift
+++ Capstone/Controllers/ProfileViewControllers/FAQViewController.swift
@@ -14,10 +14,10 @@ struct FAQInfo {
     
     static func loadFAQs() -> [FAQInfo] {
         return [
-            FAQInfo(title: "How am I supposed to use the job history?", description: "Job History is meant a \"master resume\" of sorts, allowing you to keep track of your entire job history. To be used when creating new resumes, applying to jobs, or simply keeping track of your career over time"),
-            FAQInfo(title: "Where does all of this info go?", description: "We keep a private, secure server on firebase for user data"),
-            FAQInfo(title: "What if I don't want this info uploaded?", description: "We are working on an update to allowe the user to keep all data local"),
-            FAQInfo(title: "What is a STAR story?", description: "A STAR story is an example of how you should be answering behavioral interview questions. The general structure of the answer should follow: Situation, Task, Action, Result")
+            FAQInfo(title: "How am I supposed to use the job history feature?", description: "Job History is meant to a \"master resume\" of sorts by allowing you to keep track of your entire job history.\nYou can use your logged job history as reference when creating new resumes, applying to jobs, or simply keeping track of your career over time."),
+            FAQInfo(title: "What is a STAR story?", description: "A STAR story is an example of how you should be answering behavioral interview questions. This method allows you to deliver a clear and impactful response to an interview question.\nThe general structure of the answer should follow: Situation, Task, Action, and Result."),
+            FAQInfo(title: "Where does all of this info go?", description: "We keep a private, secure server on firebase for user data."),
+            FAQInfo(title: "What if I don't want this info uploaded?", description: "We are working on an update to allow our users to store all of their data locally on their device.")
         ]
     }
 }
