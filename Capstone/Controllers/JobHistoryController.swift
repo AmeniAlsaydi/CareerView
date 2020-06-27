3239d389371e7bd8c15963f5ae724d5a4de2484e
Casandra <casandragrullon@gmail.com>
2020-06-27 14:21:28 -0400

    all job history cell constraints work to prevent lines being cut off

--- Capstone/Controllers/JobHistoryController.swift
+++ Capstone/Controllers/JobHistoryController.swift
@@ -30,7 +30,7 @@ class JobHistoryController: UIViewController {
     
     enum Const {
         static let closeCellHeight: CGFloat = 200
-        static let openCellHeight: CGFloat = 630
+        static let openCellHeight: CGFloat = 640
     }
     
     var cellHeights = [CGFloat]()
