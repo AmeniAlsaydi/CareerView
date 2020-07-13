8c367c702256bafb8852ecb315a26247764c8674
GregKeeley <GregoryKeeley@pursuit.org>
2020-07-13 14:03:49 -0400

    Formatted and cleaned up code in Views files

--- Capstone/Views/HairlineView.swift
+++ Capstone/Views/HairlineView.swift
@@ -11,10 +11,8 @@ import UIKit
 class HairlineView: UIView {
     override func awakeFromNib() {
         //guard let backgroundColor = self.backgroundColor?.cgColor else { return }
-        
         self.layer.borderColor = AppColors.primaryBlackColor.cgColor
         self.layer.borderWidth = (1.0 / UIScreen.main.scale) / 2;
         self.backgroundColor = UIColor.clear
     }
 }
-
